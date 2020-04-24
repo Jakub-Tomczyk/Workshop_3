271c48c4f1c8bd8ba2a7e3f93fdb915e972ce81b
Jakub Tomczyk <tomczyk.jakub@ymail.com>
2020-04-24 15:07:25 +0200

    updated .gitignore

---
+++ src/main/java/pl/coderslab/workshop3/model/User.java
@@ -0,0 +1,70 @@
+package pl.coderslab.workshop3.model;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+
+
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    private Integer group_id;
+
+    public User(){}
+
+    public User(String username, String email, String password, Integer group_id) {
+        this.username = username;
+        this.email = email;
+        this.group_id = group_id;
+        hashPassword(password);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(Integer group_id) {
+        this.group_id = group_id;
+    }
+    public void hashPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    @Override
+    public String toString(){
+        return id + " " + username + " " + email + " " + password + " " + group_id;
+    }
+}
